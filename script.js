2125a2fdc6e9b92368545d135ccd321900da69fa
Student <student@compass.local>
2025-12-19 11:05:49 +0600

    edit

---
+++ script.js
@@ -0,0 +1,34 @@
+const btnAbout = document.getElementById('btn-about')
+const btnStudy = document.getElementById('btn-study')
+const btnHobby = document.getElementById('btn-hobby')
+
+const hobbies = ['draw', 'music', 'cook'];
+
+
+
+const customAlert = (message) => {
+    const overlay = document.createElement('div');
+    overlay.className = 'overlay';
+    document.body.append(overlay);
+
+    const contentDiv = document.createElement('div');
+    contentDiv.className = 'content';
+    overlay.append(contentDiv);
+
+    const messageH3 = document.createElement('h3');
+    messageH3.textContent = message;
+    contentDiv.append(messageH3);
+
+     overlay.addEventListener('click', (event) =>{
+        if (event.target.className === 'overlay'){
+            overlay.remove();
+        }
+    })
+}
+
+btnAbout.addEventListener('click', () => {
+     customAlert('my name is Polina, Im 17. my birth 5 september 2008. i have a dog and 4 cats.')
+})
+btnStudy.addEventListener('click', () => {
+    customAlert('Im study in Compass College on IT and product desing depatment')
+})
